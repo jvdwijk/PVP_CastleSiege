581ed9ffd2be1778fafe5bc7fbb6034ebed965c3
CoenSteuns <c.steuns@gmail.com>
2019-03-08 09:19:04 +0100

    Statemachine-ish turn manager

---
+++ Assets/Scripts/Turns/Turn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : MonoBehaviour
+{
+    [SerializeField]
+    private Team _team;
+
+    public Team Team => _team;
+
+    public int MoveAmount { get; private set; }
+
+    public void StartTurn(){
+        //TODO implement turn logic
+    }
+
+    public void StopTurn(){
+    }
+    
+}
---
+++ Assets/Scripts/Turns/TurnManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    [SerializeField]
+    private Turn[] _startTurns;
+
+    private Dictionary<Team, Turn> _turns = new Dictionary<Team, Turn>();
+    
+    public Turn CurrentTurn { get; private set;}
+
+    public event Action<Turn> OnTurnChanged;
+
+    private void Awake() {
+        foreach (var turn in _startTurns){
+            if (turn)
+                AddTeam(turn);
+        }
+    }
+
+    public void AddTeam(Turn turn){
+        var team = turn.Team;
+        if (_turns.ContainsKey(team))
+            return;
+
+        _turns.Add(team, turn);
+    }
+
+    public void RemoveTeam(Team team){
+        if (_turns.ContainsKey(team))
+            return;
+
+        _turns.Remove(team);
+    }
+
+    public Turn GetTurn(Team team){
+        if(!_turns.ContainsKey(team))
+            return null;
+
+        return _turns[team];
+    }
+
+    public void SetTurn(Team team){
+
+        if (!_turns.ContainsKey(team))
+            return;
+
+        CurrentTurn?.StopTurn();
+
+        CurrentTurn = _turns[team];
+
+        CurrentTurn?.StartTurn();
+
+        OnTurnChanged?.Invoke(CurrentTurn);
+    }
+
+    public void NextTurn(){
+        
+        Team nextTurn;
+        if (GetNextAvailibleTeam(out nextTurn)) {
+            SetTurn(nextTurn);
+            return;
+        }
+
+        Debug.LogError("No other team available!");
+    }
+
+    private bool GetNextAvailibleTeam(out Team nexTeam){
+        var values = Enum.GetValues(typeof(Team));
+        var currentTeamNumber = CurrentTurn ? (int)CurrentTurn.Team : -1;
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            currentTeamNumber++;
+            currentTeamNumber = (int)Mathf.Repeat(currentTeamNumber, values.Length);
+
+            var newTeam = (Team)currentTeamNumber;
+
+            if (!_turns.ContainsKey(newTeam))
+                continue;
+
+            nexTeam = newTeam;
+            return true;
+        }
+        nexTeam = 0;
+        return false;
+    }
+}
+
+
