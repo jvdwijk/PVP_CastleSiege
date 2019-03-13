efa49b9845b20e078c7c04c3b96621c916bf0cf3
Justin <justinvg3@gmail.com>
2019-03-13 13:36:18 +0100

    Main Menu

---
+++ Assets/Scripts/Menu/EnabledSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnabledSwitch : MonoBehaviour
+{
+public GameObject[] menuObjects;
+
+      public void Switch(int menuNumber)
+    {
+        menuObjects[menuNumber].SetActive(!menuObjects[menuNumber].activeSelf);
+    }
+
+    public void DisableAll()
+    {
+        foreach(GameObject menuObject in menuObjects)
+        {
+            menuObject.SetActive(false);
+        }
+    }
+}
