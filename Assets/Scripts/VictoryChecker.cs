dc58a441066387a1236c3255a6c668c904b4fbae
unknown <c.steuns@gmail.com>
2019-03-14 19:38:45 +0100

    Victory checker

---
+++ Assets/Scripts/VictoryChecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryChecker : MonoBehaviour
+{
+
+    private int pawnAmount = 4;
+
+    [SerializeField]
+    private MultiPawnTile[] finalTiles;
+
+    public Action<Team> GameWon;
+
+    private void Awake() {
+        foreach (var tile in finalTiles)
+        {
+            tile.OnNewPawnEnter += (pawn) => NewPawnEnterFinalTile(tile);
+        }
+    }
+
+    private void NewPawnEnterFinalTile(MultiPawnTile tile){
+        if (tile.PawnAmount != pawnAmount)
+            return;
+
+        GameWon?.Invoke(tile.Pawn.PawnTeam);
+    }
+
+}
