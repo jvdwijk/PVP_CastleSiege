2aeff3102907de418341738986404fff7846b165
unknown <c.steuns@gmail.com>
2019-03-18 23:36:03 +0100

    Power up GUI

---
+++ Assets/Scripts/UI/PowerUpUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerUpUI : MonoBehaviour
+{
+    [SerializeField]
+    private Image powerupIconDisplay;
+    
+    public void SetPowerupImage(Sprite image){
+        powerupIconDisplay.color = Color.white;
+        powerupIconDisplay.sprite = image;
+    }
+
+    public void ResetImage(){
+        powerupIconDisplay.color = new Color(0,0,0,0);
+    }
+
+}
