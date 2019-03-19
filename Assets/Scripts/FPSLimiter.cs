b046ba11d041211a3692f3167646ffd7ad631139
jvdwijk <joeyvdwijk@gmail.com>
2019-03-19 13:33:42 +0100

    fps limiter into main scene

--- Assets/Scripts/FPSLimiter.cs
+++ Assets/Scripts/FPSLimiter.cs
@@ -6,23 +6,21 @@ using UnityEngine.UI;
 public class FPSLimiter : MonoBehaviour
 {
     public int target = 30;
-     public int avgFrameRate;
-    public Text display_Text;
-      
-      void Awake()
-      {
-          QualitySettings.vSyncCount = 0;
-          Application.targetFrameRate = target;
-      }
-      
-      void Update()
-      {
+    public int avgFrameRate;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = target;
+    }
+
+    void Update()
+    {
         float current = 0;
         current = (int)(1f / Time.unscaledDeltaTime);
         avgFrameRate = (int)current;
-        display_Text.text = avgFrameRate.ToString() + " FPS";
 
-          if(Application.targetFrameRate != target)
-              Application.targetFrameRate = target;
-      }
+        if (Application.targetFrameRate != target)
+            Application.targetFrameRate = target;
+    }
 }
