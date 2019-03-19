71d4ff7f36a2e920d07f6545cccabe6de52312cc
Justin <justinvg3@gmail.com>
2019-03-19 12:46:23 +0100

    FPS limiter script (Requires polishing)

---
+++ Assets/Scripts/FPSLimiter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSLimiter : MonoBehaviour
+{
+    public int target = 30;
+     public int avgFrameRate;
+    public Text display_Text;
+      
+      void Awake()
+      {
+          QualitySettings.vSyncCount = 0;
+          Application.targetFrameRate = target;
+      }
+      
+      void Update()
+      {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        display_Text.text = avgFrameRate.ToString() + " FPS";
+
+          if(Application.targetFrameRate != target)
+              Application.targetFrameRate = target;
+      }
+}
