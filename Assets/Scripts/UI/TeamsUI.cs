c2f5e7c9c0b237496fe27a979f45caff44155972
jvdwijk <joeyvdwijk@gmail.com>
2019-03-21 00:14:35 +0100

    Teams UI Implemented

---
+++ Assets/Scripts/UI/TeamsUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamsUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] teams;
+
+    public void UpdateUI(int playerAmount){
+        for (int i = 0; i < teams.Length; i++)
+        {
+            if(i > playerAmount - 1)
+                teams[i].SetActive(false);
+        }
+    }
+}
